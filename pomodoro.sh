b37fc3b42919ee4a1bf356981d736f0052f4528c
John Nduli <yohanaizraeli@gmail.com>
2018-12-13 08:09:59 +0300

    Add initial pomodoro script

---
+++ pomodoro.sh
@@ -0,0 +1,67 @@
+#!/bin/bash
+
+SOUNDFILE='/usr/share/sounds/freedesktop/stereo/complete.oga'
+
+WORK=25
+REST=5
+SLEEP=1m
+
+play_notification () {
+    paplay $SOUNDFILE
+}
+
+clear_line () {
+    printf "\033[1A"  # move cursor one line up
+    printf "\033[K"   # delete till end of line
+}
+
+count_down () { 
+    for i in $(eval echo "{1..$WORK}")
+    do
+        echo "Time spent is $i minutes"
+        sleep $SLEEP
+        clear_line
+    done
+}
+
+rest () {
+    for i in $(eval echo "{1..$REST}")
+    do
+        echo "Rested for $i minutes"
+        sleep $SLEEP
+        clear_line
+    done
+}
+
+chiming_with_input () {
+    echo "Press q to stop chiming, and start break"
+    while true
+    do
+        play_notification
+        read -t 0.25 -N 1 input
+        if [[ $input = "q" ]] || [[ $input = "Q" ]]; then
+            echo
+            break
+        fi
+    done
+}
+single_pomodoro_run () {
+    echo "Starting pomodoro $1"
+    count_down
+    chiming_with_input
+    rest
+    chiming_with_input
+}
+
+# Infinite loop
+START=1
+while true
+do
+    single_pomodoro_run $START
+    START=$((START+1))
+    clear_line
+    clear_line
+    clear_line
+    clear_line
+    clear_line
+done
