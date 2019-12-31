6085e67c45a9a0de3fde113e7e015fb13ceb5be3
John Nduli <yohanaizraeli@gmail.com>
2019-12-31 13:10:28 +0300

    Add debug mode (closes #6)

--- pomodoro.sh
+++ pomodoro.sh
@@ -7,6 +7,7 @@ SOUNDFILE='alarm.oga'
 
 WORK=25
 REST=5
+SECS_IN_MINUTE=60
 LOG_DIR='.logs/'
 LOG_FILENAME=$LOG_DIR$(date +"%F").log
 
@@ -31,12 +32,12 @@ count_down () {
     # Takes two parameters
     # $1 is time in minutes
     # $2 is messages to prepend
-    secs_to_count_down=$(($1*60))
+    secs_to_count_down=$(($1*$SECS_IN_MINUTE))
     SECONDS=0 
     PRINTED_MINUTES=0
     echo -e "$2 0 minutes"
     while (( SECONDS <= secs_to_count_down )); do    # Loop until interval has elapsed.
-        minutes=$((SECONDS/60))
+        minutes=$((SECONDS/$SECS_IN_MINUTE))
         if [[ $PRINTED_MINUTES != "$minutes" ]];then
             PRINTED_MINUTES=$minutes
             clear_line
@@ -133,17 +134,21 @@ pomodoro.sh:
  -p <arg>: Set time for actual work
  -r <arg>: Set time for rest
  -l: Daily retrospection (Show work done during the day)
+ -d: debug mode (The time counter uses seconds instead of minutes)
 EOF
 }
 
 options () {
-    while getopts "p:r:lh" OPTION; do
+    while getopts "p:r:dlh" OPTION; do
         case $OPTION in
             p)
                 WORK=$OPTARG
                 ;;
             r)
                 REST=$OPTARG
+                ;;
+            d)
+                SECS_IN_MINUTE=1
                 ;;
             l)
                 view_logs
