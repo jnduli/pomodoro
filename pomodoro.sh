d99b65cc155cf2041d3e51fc2b4a00d7557c41b3
John Nduli <yohanaizraeli@gmail.com>
2020-01-24 11:48:41 +0300

    Set up readonly and local variables properly

--- pomodoro.sh
+++ pomodoro.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 #
 # Runs pomodoro with specified work duration and rest
+#
+# For countdown the SECONDS (see man bash) variable is used
 
 scriptDir=$(dirname -- "$(readlink -f -- "${BASH_SOURCE[0]}")")
 cd "$scriptDir" || exit
 
-SOUNDFILE='alarm.oga'
+readonly SOUNDFILE='alarm.oga'
 
 WORK=25
 REST=5
@@ -13,7 +15,6 @@ SECS_IN_MINUTE=60
 LOG_DIR="$HOME/.pomodoro/"
 FILENAME="$(date +"%F").log"
 LOG_FILENAME="$LOG_DIR$FILENAME"
-CONTINUE=false
 
 play_notification () {
     paplay $SOUNDFILE
@@ -25,7 +26,7 @@ play_notification () {
 # Returns
 #   None
 clear_line () {
-    lines=1
+    local lines=1
     if [ -n "$1" ]; then
         lines=$1
     fi
@@ -37,31 +38,35 @@ clear_line () {
 }
 
 # Counts down from time t until 0 minutes
+# Globals:
+#   SECS_IN_MINUTE
+#   SECONDS
 # Arguments:
 #   time in minutes ($1)
 #   message ($2)
 #   b (Optional $3): the number of times break has been avoided
 count_down () {
-    secs_to_count_down=$(($1*SECS_IN_MINUTE))
+    local secs_to_count_down=$(($1*SECS_IN_MINUTE))
+    local message=$2
+    local printed_minutes=0
+    echo -e "$message $printed_minutes minutes"
     SECONDS=0 
     if [ -n "$3" ]; then
         SECONDS=$(($1*SECS_IN_MINUTE*$3))
         secs_to_count_down=$((($3+1)*$1*SECS_IN_MINUTE))
     fi
-    PRINTED_MINUTES=0
-    echo -e "$2 0 minutes"
     while (( SECONDS <= secs_to_count_down )); do    # Loop until interval has elapsed.
         minutes=$((SECONDS/SECS_IN_MINUTE))
-        if [[ $PRINTED_MINUTES != "$minutes" ]]; then
-            PRINTED_MINUTES=$minutes
+        if [[ $printed_minutes != "$minutes" ]]; then # stops stuttering of screen by second by second update
+            printed_minutes=$minutes
             clear_line
-            echo -e "$2 $PRINTED_MINUTES minutes"
+            echo -e "$message $printed_minutes minutes"
         fi
         read -r -t 0.25 -N 1 input
         if [[ ${input^^} = "P" ]]; then
-            PAUSEDTIME=$SECONDS
+            local pausedtime=$SECONDS
             pause_forever
-            SECONDS=$PAUSEDTIME
+            SECONDS=$pausedtime
         elif [[ ${input^^} = "Q" ]]; then
             break
         fi
@@ -102,7 +107,9 @@ rest () {
 
 # Plays notification until key is pressed
 # Globals:
-#   CONTINUE
+#   SECONDS
+# Returns
+#   0 for True, 1 for False
 chiming_with_input () {
     cat <<EOF
 Press q to stop chiming and start break
@@ -110,6 +117,7 @@ Press c to continue with task
 EOF
     echo ""
     SECONDS=0
+    local should_continue
     while true; do
         play_notification
         read -r -t 0.25 -N 1 input
@@ -117,35 +125,34 @@ EOF
         clear_line
         echo "Chiming duration: $((duration / 60)) min $((duration % 60)) sec"
         if [[ ${input^^} = "Q" ]]; then
-            CONTINUE=false
+            should_continue=1
             break
         fi
         if [[ ${input^^} = "C" ]]; then
-            CONTINUE=true
+            should_continue=0
             break
         fi
     done
     clear_line 3
+    return $should_continue
 }
 
 # Runs one complete pomodoro i.e. with work and rest
-# Globals:
-#   CONTINUE
 # Arguments:
 #   n : The current pomodoro number
 single_pomodoro_run () {
     echo "Pomodoro $1"
-    START_TIME=$(date +%R)
-    work
-    chiming_with_input
-    WORK_CONT=0
-    while $CONTINUE; do
-        WORK_CONT=$((WORK_CONT+1))
-        work $WORK_CONT
+    local start_time=$(date +%R)
+    local work_continue=0
+    local work_no=0
+    while ((work_continue == 0)); do
+        work $work_no
         chiming_with_input
+        work_continue=$? # result from previous command
+        work_no=$((work_no+1))
     done
-    END_TIME=$(date +%R)
-    log "$1" "$START_TIME - $END_TIME"
+    local end_time=$(date +%R)
+    log "$1" "$start_time - $end_time"
     rest
     chiming_with_input
 }
@@ -238,14 +245,14 @@ main () {
     options "$@"
     rename_window_in_tmux
     # infinite loop
-    START=1
+    local pomodoro_count=1
     if [ -f "$LOG_FILENAME" ]; then
-        START=$(wc -l < "$LOG_FILENAME")
-        START=$((START+1))
+        pomodoro_count=$(wc -l < "$LOG_FILENAME")
+        pomodoro_count=$((pomodoro_count+1))
     fi
     while true; do
-        single_pomodoro_run $START
-        START=$((START+1))
+        single_pomodoro_run $pomodoro_count
+        pomodoro_count=$((pomodoro_count+1))
     done
 }
 
