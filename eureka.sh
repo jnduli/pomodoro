ac49ab0a328b45a108ec5ce4c1007165bff15121
John Nduli <yohanaizraeli@gmail.com>
2020-12-12 09:58:02 +0300

    clean up eureka

--- eureka.sh
+++ eureka.sh
@@ -1,40 +1,10 @@
-# Check if its a git repo
-# check if one can commit by checking for user.name user.email
-# default folder location for this
-# using $EDITOR for editting
-# using $PAGER to view the file
-# Formatting of the markdown file too
-
-GIT_REPO_DIRECTORY="$HOME/projects/ideas"
-
-if ! [ -x "$(command -v git)" ]; then
-  echo 'Error: git is not installed.' >&2
-  exit 1
-fi
-
-
-# mkdir -p "$GIT_REPO_DIRECTORY"
-# cd "$GIT_REPO_DIRECTORY" || exit
-
-# # TODO: Better set up flow
-# if ! [ -d .git ]; then
-#     echo "Setting up"
-#     echo "What's your email"
-#     read -r email
-#     echo "Whats your user name"
-#     read -r username
-#     git init
-#     git config user.name "$username"
-#     git config user.email "$email"
-#     echo "#IDEAS" >> README.md
-#     echo "Done setting up"
-#     exit 1
-# fi;
-
+#!/bin/bash
 
 show_help() {
     cat <<EOF
 Eureka v0.0.1 stores ideas I get on the fly
+
+Requirements: sqlite3
 
 Options are:
 -v --version: show version
@@ -44,41 +14,47 @@ Options are:
 EOF
 }
 
-add_idea() {
-    # Process Flow
-    echo "Summary of Idea"
-    read -r idea_summary
-    { echo "idea: $idea_summary"
-    "notes: you can edit this or remove it if there are no notes"
-    ""
-    } >> README.md
-    $EDITOR README.md
-    git add README.md
-    git commit -m "$idea_summary"
+
+SOFTWARE_REQUIREMENTS=(git sqlite3)
+SQLITE_DBASE="$HOME/projects/pomodoro/eureka.db"
+
+
+# Check if software requirements defined in SOFTWARE_REQUIREMENTS array are installed
+# If any is not installed, the whole program will exit
+check_requirements_met() {
+    requirement_not_found=0
+    for requirement in "${SOFTWARE_REQUIREMENTS[@]}"
+    do
+        if ! [ -x "$(command -v "$requirement")" ]; then
+            echo "Error: ${requirement} is not installed" >&2
+            requirement_not_found=1
+        fi
+    done
+
+    if [ $requirement_not_found == 1 ]; then
+        exit 1
+    fi
 }
 
-view_ideas() {
-    $PAGER README.md
-}
 
 create_sqlite_table() {
     TABLE_QUERY="CREATE TABLE IF NOT EXISTS eureka (id INTEGER PRIMARY KEY, created_utc DATETIME DEFAULT (DATETIME('now')), modified_utc DATETIME DEFAULT (DATETIME('now')), summary TEXT, description TEXT)"
-    sqlite3 eureka.db "$TABLE_QUERY"
+    sqlite3 "$SQLITE_DBASE" "$TABLE_QUERY"
 }
 
-add_sqlite_idea() {
+add_idea() {
     # Process Flow
     echo "Summary of Idea"
     read -r idea_summary
     echo "Details of Idea"
     read -r idea_details
     INSERT_QUERY="INSERT INTO eureka (summary, description) VALUES (\"$idea_summary\", \"$idea_details\")"
-    sqlite3 eureka.db "$INSERT_QUERY"
+    sqlite3 "$SQLITE_DBASE" "$INSERT_QUERY"
 }
 
-view_sqlite_ideas() {
+view_ideas() {
     SELECT_QUERY="SELECT * from eureka ORDER BY created_utc DESC"
-    sqlite3 -header -column eureka.db "$SELECT_QUERY"
+    sqlite3 -header -column "$SQLITE_DBASE" "$SELECT_QUERY" | $PAGER
 }
 
 options_handling() {
@@ -86,8 +62,8 @@ options_handling() {
     do
         case $i in
             -h | --help) show_help; return;;
-            -a | --add) add_sqlite_idea; return;;
-            -v | --view) view_sqlite_ideas; return;;
+            -a | --add) add_idea; return;;
+            -v | --view) view_ideas; return;;
             *) show_help; return ;;
         esac
     done
@@ -95,5 +71,6 @@ options_handling() {
     show_help
 }
 
+check_requirements_met
 create_sqlite_table
 options_handling "$@"
