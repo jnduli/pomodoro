b055fcba75938a470fa3a9730b313b03e93a73ff
John Nduli <yohanaizraeli@gmail.com>
2022-06-13 09:01:20 +0300

    add timer script to pomodoro

---
+++ timer.sh
@@ -0,0 +1,66 @@
+# SECONDS=0
+# echo "seconds"
+# echo $SECONDS
+
+readonly SOUNDFILE='alarm.oga'
+
+
+notify () {
+    echo "Press q to stop sound"
+    while true; do
+        paplay $SOUNDFILE
+        read -r -t 1.0 -N 1 input || true
+        if [[ ${input^^} == "Q" ]]; then
+            break
+        fi
+    done
+}
+
+show_help () {
+    cat <<EOF
+Copyright (C) 2022: John Nduli K.
+
+timer.sh 
+starts a timer from the terminal, ringing after time is done.
+
+-h: Show help
+times: times passed to sleep
+EOF
+}
+
+options () {
+    while getopts "t:h" OPTION; do
+        case $OPTION in
+            h)  show_help
+                exit 1
+                ;;
+            t) echo "Timer set for $OPTARG" && sleep $OPTARG && notify
+                exit 1
+                ;;
+            \?) echo "Invalid option: -$OPTARG" >&2
+                exit 1
+                ;;
+        esac
+    done
+}
+
+
+
+options "$@"
+
+
+
+# timer_seconds() {
+#     echo "$@"
+#     local seconds=$1
+#     SECONDS=0
+
+#     while [[ SECONDS -lt seconds ]]; do
+#         echo "seconds passed: $SECONDS"
+#         sleep 1s
+#     done
+
+#     echo "done counting"
+# }
+
+# timer_seconds 300
